367d576d2947b936cc113d06fb013ab5975aab27
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-10-14 19:03:09 +0300

    Autowiring completed

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
@@ -1,19 +1,16 @@
 package com.cydeo.streotype_annotation;
 
 import com.cydeo.streotype_annotation.config.PcConfig;
-import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
-
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
+        ApplicationContext container =new AnnotationConfigApplicationContext(PcConfig.class);
 
-        Monitor theMonitor = container.getBean(Monitor.class);
-        System.out.println(theMonitor.getSize());
+        PC myPc = container.getBean(PC.class);
 
-
+        System.out.println(myPc.getTheCase().getDimensions().getDepth());
     }
 }
