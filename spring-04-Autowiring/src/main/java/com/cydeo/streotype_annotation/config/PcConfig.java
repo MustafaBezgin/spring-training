c41b3b3bbc7a6b662de9e6ac3328dc26f5988e2b
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-10-15 15:33:49 +0300

    Error fixed (my solution)

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -1,10 +1,17 @@
 package com.cydeo.streotype_annotation.config;
 
+import com.cydeo.streotype_annotation.casefactory.Dimensions;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.cydeo.streotype_annotation")
 public class PcConfig {
+
+    @Bean
+    public Dimensions dimensions(){
+        return new Dimensions(50, 10, 10);
+    }
 
 }
