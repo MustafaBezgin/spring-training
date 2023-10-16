5820325e2a3b032b7f1b92510b9754765b7bc32d
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-10-16 15:08:02 +0300

    Value annotation, application.properties

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.cydeo.service;
 
+import com.cydeo.config.AppConfigData;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
@@ -14,13 +15,14 @@ public class CommentService {
      */
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
+    private final AppConfigData appConfigData;
 
     // By creating constructor, we automatically add @Autowired annotation as well
 
-
-    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
     }
 
     public void publishComment(Comment comment){
@@ -29,6 +31,12 @@ public class CommentService {
         // Send email
         commentNotificationProxy.sendComment(comment);
 
+    }
+
+    public void printConfigData(){
+        System.out.println(appConfigData.getUserName());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
     }
 
 }
