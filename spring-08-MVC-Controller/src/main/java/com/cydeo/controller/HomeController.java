83354009268af112ffc50891f3c519cac1e133ff
Mustafa Bezgin <av.mustafabezgin@gmail.com>
2023-10-16 21:12:38 +0300

    MVC Controller

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -6,9 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home") // it is named "end point" and it will concatenate with url
     public String getHomePage() {
         return "home.html";
     }
 
+    @RequestMapping("/abcd")
+    public String getHomePage2() {
+        return "home.html";
+    }
+
+    @RequestMapping // nothing or only "/" to direct us default page
+    public String getHomePage3() {
+        return "home.html";
+    }
+
+    @RequestMapping({"/apple", "/orange"})
+    public String getHomePage4() {
+        return "home.html";
+    }
 }
